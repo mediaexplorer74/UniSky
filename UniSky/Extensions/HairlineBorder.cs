76a712216db6c154bfba52c52d06018e073202b8
Thomas May <wamwoowam@gmail.com>
2024-12-03 12:49:57 +0000

    Hairline border improvements

--- UniSky/Extensions/HairlineBorder.cs
+++ UniSky/Extensions/HairlineBorder.cs
@@ -24,15 +24,27 @@ namespace UniSky.Extensions
         public static readonly DependencyProperty ThicknessProperty =
             DependencyProperty.RegisterAttached("Thickness", typeof(Thickness), typeof(HairlineBorder), new PropertyMetadata(new Thickness(), OnThicknessPropertyChanged));
 
+        private static List<WeakReference<DependencyObject>> Elements { get; set; } = [];
+        private static DisplayInformation DisplayInfo { get; set; }
+
         private static void OnThicknessPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is not (Grid or StackPanel or Control or Border))
                 return;
 
-            var newValue = (Thickness)(e.NewValue);
+            Elements.Add(new WeakReference<DependencyObject>(d));
 
-            // TODO: this should be faster
-            var info = DisplayInformation.GetForCurrentView();
+            if (DisplayInfo == null)
+            {
+                Initialize();
+            }
+
+            var newValue = (Thickness)(e.NewValue);
+            Apply(d, newValue, DisplayInfo);
+        }
+
+        private static void Apply(DependencyObject d, Thickness newValue, DisplayInformation info)
+        {
             var hairlineThickness = (1.0 / (info.LogicalDpi / 96.0));
             var thickness = new Thickness(
                 newValue.Left != 0 ? hairlineThickness : 0,
@@ -54,6 +66,23 @@ namespace UniSky.Extensions
                 case StackPanel s:
                     s.BorderThickness = thickness;
                     break;
+            }
+        }
+
+        private static void Initialize()
+        {
+            DisplayInfo = DisplayInformation.GetForCurrentView();
+            DisplayInfo.DpiChanged += OnDpiChanged;
+        }
+
+        private static void OnDpiChanged(DisplayInformation sender, object args)
+        {
+            foreach (var item in Elements)
+            {
+                if (!item.TryGetTarget(out var element) || element.GetValue(ThicknessProperty) is not Thickness thickness)
+                    continue;
+
+                Apply(element, thickness, sender);
             }
         }
     }
