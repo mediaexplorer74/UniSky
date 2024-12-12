37c328790aed4fd633682c1275bdfed9c0e13216
Thomas May <wamwoowam@gmail.com>
2024-12-12 23:38:51 +0000

    cleanup

--- UniSky/Converters/Static.cs
+++ UniSky/Converters/Static.cs
@@ -1,18 +1,17 @@
-﻿namespace UniSky.Converters
+﻿namespace UniSky.Converters;
+
+public class Static
 {
-    public class Static
-    {
-        public static bool Equals(int a, int b)
-            => a == b;
-        public static bool AtLeast(int a, int b)
-            => a >= b;
-        public static bool Not(bool x)
-            => !x;
-        public static bool NotNull(object x)
-            => x is not null;
-        public static bool NullOrWhiteSpace(string s)
-            => string.IsNullOrWhiteSpace(s);
-        public static bool NotNullOrWhiteSpace(string s)
-            => !string.IsNullOrWhiteSpace(s);
-    }
+    public static bool Equals(int a, int b)
+        => a == b;
+    public static bool AtLeast(int a, int b)
+        => a >= b;
+    public static bool Not(bool x)
+        => !x;
+    public static bool NotNull(object x)
+        => x is not null;
+    public static bool NullOrWhiteSpace(string s)
+        => string.IsNullOrWhiteSpace(s);
+    public static bool NotNullOrWhiteSpace(string s)
+        => !string.IsNullOrWhiteSpace(s);
 }
