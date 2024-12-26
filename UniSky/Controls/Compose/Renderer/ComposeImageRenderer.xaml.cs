44fd7fa265d5d12b388f614febd276d82a54627b
Thomas May <wamwoowam@gmail.com>
2024-12-26 16:59:24 +0000

    Update ComposeImageRenderer.xaml.cs

--- UniSky/Controls/Compose/Renderer/ComposeImageRenderer.xaml.cs
+++ UniSky/Controls/Compose/Renderer/ComposeImageRenderer.xaml.cs
@@ -5,27 +5,25 @@ using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+namespace UniSky.Controls.Compose.Renderer;
 
-namespace UniSky.Controls.Compose.Renderer
+public sealed partial class ComposeImageRenderer : UserControl
 {
-    public sealed partial class ComposeImageRenderer : UserControl
+    public ComposeImageRenderer()
     {
-        public ComposeImageRenderer()
-        {
-            this.InitializeComponent();
-        }
-
-        public async Task LoadImage(RandomAccessStreamReference reference)
-        {
-            using var bmp = await reference.OpenReadAsync();
-            var decoder = await BitmapDecoder.CreateAsync(bmp);
-            using var softwareBmp = await decoder.GetSoftwareBitmapAsync(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
-
-            var softwreBitmapSource = new SoftwareBitmapSource();
-            await softwreBitmapSource.SetBitmapAsync(softwareBmp);
-
-            Image.Source = softwreBitmapSource;
-        }
+        this.InitializeComponent();
     }
-} 
+
+    public async Task LoadImage(RandomAccessStreamReference reference)
+    {
+        using var bmp = await reference.OpenReadAsync();
+        var decoder = await BitmapDecoder.CreateAsync(bmp);
+        using var softwareBmp = await decoder.GetSoftwareBitmapAsync(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+
+        var softwreBitmapSource = new SoftwareBitmapSource();
+        await softwreBitmapSource.SetBitmapAsync(softwareBmp);
+
+        Image.Source = softwreBitmapSource;
+    }
+}
+
